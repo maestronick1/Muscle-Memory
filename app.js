7b44bb18a80699bcbcc3a7631b36d21e173e951b
Nicholas Phillips <nicholasphillips@Nicholass-MacBook-Air.local>
2020-07-17 14:09:49 -0700

    muscle memory

---
+++ app.js
@@ -0,0 +1,21 @@
+var string ("nick");
+console.log(string);
+
+
+let number = (12);
+console.log=(number)
+
+var cars = ["Saab", "Volvo", "BMW", "Toyota"];
+console.log(cars)
+
+let todayIsSat = false;
+console.log(todayIsSat) 
+
+
+let user = {
+    name: "Nick",
+    town: "Hughson"
+    food: "pizza"
+    age: 36
+    isYoung: false
+  };
