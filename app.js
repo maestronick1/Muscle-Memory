8d5a16b2f0bc8fbb13d2438bcbe3171fc1b0dc12
Nicholas Phillips <nicholasphillips@Nicholass-MacBook-Air.local>
2020-07-17 14:15:23 -0700

    MM

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ console.log(string);
 
 
 let number = (12);
-console.log=(number)
+console.log=(number);
 
 var cars = ["Saab", "Volvo", "BMW", "Toyota"];
-console.log(cars)
+console.log(cars);
 
 let todayIsSat = false;
-console.log(todayIsSat) 
+console.log(todayIsSat);
 
 
 let user = {
     name: "Nick",
-    town: "Hughson"
-    food: "pizza"
-    age: 36
+    town: "Hughson",
+    food: "pizza",
+    age: "36",
     isYoung: false
   };
