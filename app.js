6c9b8c6d3633e906d69a8d30079c5d4d20745e52
Nicholas Phillips <nicholasphillips@Nicholass-MacBook-Air.local>
2020-07-17 14:51:56 -0700

    functions

--- app.js
+++ app.js
@@ -19,3 +19,53 @@ let user = {
     age: "36",
     isYoung: false
   };
+
+
+  //functions
+
+  function printName (){   
+      let name= "Nick";
+      console.log(printName)
+
+  }
+  var fruits = ["Banana", "Orange", "Apple", "Mango"];
+  var x = fruits.pop(); 
+
+  function greeting() {
+    console.log("Hello, World!");
+
+   //pass in array
+    function displayCoolCar(array)
+    let result=[];
+    for let i= 0; i , array.length; i ++0 {
+        if (nbike.length > 7) {
+            result.push(cars);
+        }
+    }
+    return result
+}
+console.log*(displaCoolCar(cars));
+
+function removeFromArray(callback, array) {
+    let result = callback(array) [0];
+    console.log(result);
+}
+
+removeFromArray(displayCoolCar, cars);
+
+function printSentence(string) {
+    console.log(string);
+
+}
+
+function printPlayers(object){
+    console.log(object.players);
+    printPlayers(lakers);
+
+function printEachPlayer(object) {
+    object.player.forEach(eachplayer=> {
+        console.log(eachplayer);
+
+    });
+}    
+}
